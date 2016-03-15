c1c0a008c791f3360b6aa126d15d6a638a16cb59
EC2 Default User <ec2-user@ip-172-31-13-85.ap-northeast-1.compute.internal>
2016-03-15 14:21:22 +0000

    mantion-market scraper

--- mansion-market/scraper.py
+++ mansion-market/scraper.py
@@ -11,5 +11,19 @@ print(soup.title.string)
 # title = soup.find_one('//*[@id="contents"]/div/section/div/div[1]/h1')
 title = soup.find_one('h1', class_='title')
 print(title)
+address = soup.find_one('#contents > div > section > div > div.pullLeft > div.information2 > span.address.iconWrap > span')
+print(address)
+built_year = soup.find_one('span', class_='year')
+print(built_year)
+price = soup.find_one('', class_='')
 
+day = soup.find_one('', class_='')
+
+floor_located = soup.find_one('', class_='')
+
+layout = soup.find_one('', class_='')
+
+area = soup.find_one('', class_='')
+
+direction = soup.find_one('', class_='')
 
